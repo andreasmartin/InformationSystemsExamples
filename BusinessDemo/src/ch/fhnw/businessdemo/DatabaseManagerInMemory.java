1c9bc93cf595e821b5c712d412990fa87c2af1b5
Andreas Martin <andreas.martin@andreasmartin.ch>
2016-08-26 16:06:36 +0200

    Update

--- BusinessDemo/src/ch/fhnw/businessdemo/DatabaseManagerInMemory.java
+++ BusinessDemo/src/ch/fhnw/businessdemo/DatabaseManagerInMemory.java
@@ -3,39 +3,34 @@ package ch.fhnw.businessdemo;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class DatabaseManagerInMemory{
-    
+public class DatabaseManagerInMemory {
+
     private static final ObservableList<Customer> customerData = FXCollections.observableArrayList();
 
-    public static ObservableList getData()
-    {
-        initIfEmpty();
+    private static DatabaseManagerInMemory instance = null;
+
+    private DatabaseManagerInMemory() {
+    }
+
+    public static DatabaseManagerInMemory getInstance() {
+        if (instance == null) {
+            instance = new DatabaseManagerInMemory();
+        }
+        return instance;
+    }
+
+    public ObservableList<Customer> getCustomers() {
         return customerData;
     }
-    
-    private static void initIfEmpty()
-    {
-        customerData.add(new Customer("Hans", "Muster"));
-        customerData.add(new Customer("Ruth", "Mueller"));
-        customerData.add(new Customer("Heinz", "Kurz"));
-        customerData.add(new Customer("Cornelia", "Meier"));
-        customerData.add(new Customer("Werner", "Meyer"));
-        customerData.add(new Customer("Lydia", "Kunz"));
-        customerData.add(new Customer("Anna", "Best"));
-        customerData.add(new Customer("Stefan", "Meier"));
-        customerData.add(new Customer("Martin", "Mueller"));
-    }
-    
-    public Customer createCustomer(String firstName, String lastName)
-    {
+
+    public Customer createCustomer(String firstName, String lastName) {
         Customer customer = new Customer(firstName, lastName);
         customerData.add(customer);
         return customer;
     }
-    
-    public static void deleteInitData()
-    {
-        customerData.remove(0, 9);
+
+    public void deleteCustomer(Customer customer) {
+        customerData.remove(customer);
     }
-    
+
 }
--- BusinessDemo/src/ch/fhnw/businessdemo/Presenter.java
+++ BusinessDemo/src/ch/fhnw/businessdemo/Presenter.java
@@ -2,10 +2,16 @@ package ch.fhnw.businessdemo;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -14,16 +20,43 @@ import javafx.scene.control.TableView;
 public class Presenter implements Initializable {
 
     @FXML
-    private TableView<Customer> CustomerTable;
+    private TableView<Customer> customerTable;
     @FXML
     private TableColumn<Customer, String> firstNameColumn;
     @FXML
     private TableColumn<Customer, String> lastNameColumn;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+
+    private final DatabaseManagerInMemory databaseManagerInMemory = DatabaseManagerInMemory.getInstance();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        CustomerTable.setItems(DatabaseManagerInMemory.getData());
+        customerTable.setItems(databaseManagerInMemory.getCustomers());
         firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
         lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem menuItem = new MenuItem("delete");
+        contextMenu.getItems().add(menuItem);
+        menuItem.setOnAction((ActionEvent e) -> {
+            int selectedIndex = customerTable.getSelectionModel().getSelectedIndex();
+            if (selectedIndex >= 0) {
+                databaseManagerInMemory.deleteCustomer(customerTable.getItems().get(selectedIndex));
+            }
+        });
+
+        customerTable.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent t) -> {
+            if (t.getButton() == MouseButton.SECONDARY) {
+                contextMenu.show(customerTable, t.getScreenX(), t.getScreenY());
+            }
+        });
+    }
+
+    @FXML
+    private void handleCreateCustomer() {
+        databaseManagerInMemory.createCustomer(firstName.getText(), lastName.getText());
     }
 }
